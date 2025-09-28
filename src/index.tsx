18da2d6040bf2c92e2967029c4c8583cc9e3da36
HASSANalgabli <hassanalgabli19@gmail.com>
2025-09-28 13:42:09 +0300

    Add index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/invoice.css";
+import "./styles/print.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(<App />);
